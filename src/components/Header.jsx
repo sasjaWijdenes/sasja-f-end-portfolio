5f94a434282a09e5ee4630c1b3f5c9c2d2a72197
sasjaWijdenes <saswij88@gmail.com>
2022-11-15 16:16:10 +0000

    merging to main

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,10 @@ import ProfileWidget from "./ProfileWidget"
 const Header = ({error, setError}) => {
     return <header>
         <h1>NC GAMES</h1>
-        <nav>
-            <Link className="header-nav" style={{textDecoration: 'none'}} to='/comunity' > COMUNITY </Link>
-            <Link className="header-nav" style={{textDecoration: 'none'}} to='/' > HOME </Link>
-            <Link className="header-nav" style={{textDecoration: 'none'}} to='/reviewPage/newReview' > NEW REVIEW </Link>
+        <nav className="header-nav">
+            <Link className="header-nav-item" style={{textDecoration: 'none'}} to='/comunity' > COMUNITY </Link>
+            <Link className="header-nav-item" style={{textDecoration: 'none'}} to='/' > HOME </Link>
+            <Link className="header-nav-item" style={{textDecoration: 'none'}} to='/reviewPage/newReview' > NEW REVIEW </Link>
         </nav>
         <ProfileWidget error={error} setError={setError} />
     </header>
